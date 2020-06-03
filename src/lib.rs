42dfe9f357718b0c537390b5502f1023b9f3a9e3
Matthias Kaak <m.kaak@protonmail.ch>
2020-06-03 20:34:10 +0000

    It turned out that it is bad to return an Result

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,6 @@
 /// Canonicalizes values
 pub trait Canonicalize: Sized
 {
-    /// Type in which errors get reported
-    type ErrorType;
     /// Performs the canonicalizing
-    fn canon<E>(self) -> Result<Self, Self::ErrorType>;
+    fn canon(self) -> Self;
 }
