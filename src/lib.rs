85766300243591074b91f183ea3b5af5e8505431
Matthias Kaak <m.kaak@protonmail.ch>
2020-06-03 19:48:44 +0000

    Fixed a careless mistake

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@
 /// Canonicalizes values
 pub trait Canonicalize: Sized
 {
+    /// Type in which errors get reported
+    type ErrorType;
     /// Performs the canonicalizing
-    fn canon<E>(self) -> Result<Self, E>;
+    fn canon<E>(self) -> Result<Self, Self::ErrorType>;
 }
